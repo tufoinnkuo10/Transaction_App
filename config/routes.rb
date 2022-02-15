a1fb18fb88a0ddc37605f017129828186c6227ab
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-02-15 15:15:10 +0100

    Add nav through partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :operations
+  resources :groups
+  resources :users
+
+  root 'groups#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
