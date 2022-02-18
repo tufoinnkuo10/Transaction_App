667b400db2b0c6bac67b72b4b89416b357a1ed24
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2022-02-19 00:43:40 +0100

    add heroku host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "transcation_app_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = {
+    host: 'https://tufoin-transaction.herokuapp.com'
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
